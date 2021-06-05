07d166285ed84e62e9644b11997c27c8678699fb
Zacchari Emanuel <Zacchari.Emanuel@live.ca>
2021-06-05 01:32:29 -0400

    Added Flagellation Added Masochism AutoIgnored Default Common cards

--- theDefault/src/main/java/testMod/cards/Flagellation.java
+++ theDefault/src/main/java/testMod/cards/Flagellation.java
@@ -2,29 +2,29 @@ package testMod.cards;
 
 import basemod.AutoAdd;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 import testMod.DefaultMod;
 import testMod.characters.TheDefault;
+import testMod.powers.SmokePower;
 
 import static testMod.DefaultMod.makeCardPath;
-@AutoAdd.Ignore
-public class DefaultAttackWithVariable extends AbstractDynamicCard {
 
-    /*
-     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
-     *
-     * Special Strike: Deal 7 (*) damage times the energy you currently have.
-     */
+public class Flagellation extends AbstractDynamicCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = DefaultMod.makeID(DefaultAttackWithVariable.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String ID = DefaultMod.makeID(Flagellation.class.getSimpleName());
+    public static final String IMG = makeCardPath("Flagellation.png"); //Default IMG
+    // Use if you have a png for this card //public static final String IMG = makeCardPath("Flagellation.png");
 
     // /TEXT DECLARATION/
 
@@ -36,42 +36,43 @@ public class DefaultAttackWithVariable extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
-    private static final int COST = 1;
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_PLUS_DMG = 1;
+    private static final int COST = 0;
+    //private static final int UPGRADED_COST = 0;
 
-    public int specialDamage;
+    private static final int DAMAGE = 7;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+    private static final int MAGIC = 2;
+
+
 
     // /STAT DECLARATION/
 
-    public DefaultAttackWithVariable() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        baseDamage = DAMAGE;
 
-        isMultiDamage = true;
+    public Flagellation() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        damage = baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = MAGIC;
+
     }
+
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        // Create an int which equals to your current energy.
-        int effect = EnergyPanel.totalCount;
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        AbstractDungeon.actionManager.addToBottom(new LoseHPAction(p, p, magicNumber));
 
-        // For each energy, create 1 damage action.
-        for (int i = 0; i < effect; i++) {
-            AbstractDungeon.actionManager.addToBottom(
-                    new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn),
-                            AbstractGameAction.AttackEffect.FIRE));
-        }
     }
+
 
     // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
-
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
+
             initializeDescription();
         }
     }
--- theDefault/src/main/java/testMod/cards/Masochism.java
+++ theDefault/src/main/java/testMod/cards/Masochism.java
@@ -1,67 +1,75 @@
 package testMod.cards;
 
 import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 import testMod.DefaultMod;
+import testMod.actions.SoulHealAction;
 import testMod.characters.TheDefault;
+import testMod.powers.SmokePower;
 
 import static testMod.DefaultMod.makeCardPath;
 
-@AutoAdd.Ignore
-public class DefaultCommonSkill extends AbstractDynamicCard {
-
-    /*
-     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
-     *
-     * Defend Gain 5 (8) block.
-     */
-
+public class Masochism extends AbstractDynamicCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = DefaultMod.makeID(DefaultCommonSkill.class.getSimpleName());
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String ID = DefaultMod.makeID(Masochism.class.getSimpleName());
+    public static final String IMG = makeCardPath("Masochism.png");
 
     // /TEXT DECLARATION/
 
 
-    // STAT DECLARATION 	
+    // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int BLOCK = 5;
+
+    private static final int BLOCK = 10;
     private static final int UPGRADE_PLUS_BLOCK = 3;
+    private static final int MAGIC = 3;
 
 
     // /STAT DECLARATION/
 
 
-    public DefaultCommonSkill() {
+    public Masochism() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        baseBlock = BLOCK;
 
-        this.tags.add(CardTags.STARTER_DEFEND); //Tag your strike, defend and form (Wraith form, Demon form, Echo form, etc.) cards so that they function correctly.
+        block = baseBlock = BLOCK;
+        magicNumber = baseMagicNumber = MAGIC;
+        this.exhaustOnUseOnce = true;
+
     }
+
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, block));
+        AbstractDungeon.actionManager.addToBottom(new LoseHPAction(p, p, magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new SoulHealAction(p, this.block));
     }
 
-    //Upgraded stats.
+
+    // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
             upgradeBlock(UPGRADE_PLUS_BLOCK);
+
             initializeDescription();
         }
     }
